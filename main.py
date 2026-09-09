dbc43775f6c0b81edd3a1a2c9a197f0f8107f19b
thiagola92 <thiagola92@gmail.com>
2026-09-09 11:21:19 -0300

    Backup draft

--- main.py
+++ main.py
@@ -1,78 +1,51 @@
-import json
-import re
+import sys
 from pathlib import Path
 
 import requests
 
-RSS_URL = "https://docs.discord.com/developers/change-log/rss.xml"
-RSS_CACHE = "rss.xml"
-VERSION_FILE = "version.json"
-
-
-def get_rss() -> str:
-    rss_file = Path(RSS_CACHE)
-
-    if rss_file.exists():
-        return rss_file.read_text()
-
-    response = requests.get(RSS_URL)
-
-    if response.status_code != 200:
-        return ""
-
-    rss_file.write_text(response.text)
-
-    return rss_file.read_text()
-
-
-def get_version(rss: str) -> dict:
-    version_file = Path(VERSION_FILE)
-
-    # Delete file at any moment so we can test again.
-    if version_file.exists():
-        biggest = json.loads(version_file.read_text())
-    else:
-        biggest = {"major": 0, "minor": 0, "patch": 0, "collected": False}
-
-    for result in re.findall(r"\n +<title><!\[CDATA\[(.+)\]\]>", rss):
-        if result.startswith("Discord Social SDK"):
-            for version in re.findall(r"(\d+).(\d+).(\d+)", result):
-                major = int(version[0])
-                minor = int(version[1])
-                patch = int(version[2])
-
-                if major < biggest["major"]:
-                    continue
-                elif major > biggest["major"]:
-                    biggest["major"] = major
-                    biggest["minor"] = minor
-                    biggest["patch"] = patch
-                    continue
-
-                if minor < biggest["minor"]:
-                    continue
-                elif minor > biggest["minor"]:
-                    biggest["major"] = major
-                    biggest["minor"] = minor
-                    biggest["patch"] = patch
-                    continue
-
-                if patch < biggest["patch"]:
-                    continue
-                elif patch > biggest["patch"]:
-                    biggest["major"] = major
-                    biggest["minor"] = minor
-                    biggest["patch"] = patch
-                    continue
-
-    version_file.write_text(json.dumps(biggest))
-
-    return biggest
+RELEASES_URL = "https://discord.com/api/v9/social-sdk/releases"
+version_file = Path("version")
 
 
 def main():
-    rss = get_rss()
-    print(get_version(rss))
+    authorization = input("Pass the authorization: ")
+    response = requests.get(RELEASES_URL, headers={"Authorization": authorization})
+
+    if response.status_code != 200:
+        print("Failed to get versions", sys.stderr)
+        print(response.status_code, sys.stderr)
+        print(response.content, sys.stderr)
+        print(response.headers, sys.stderr)
+        return
+
+    content = response.json()
+    latest_version = content["latest_version"]
+
+    if latest_version == version_file.read_text():
+        print("No new version")
+        return
+
+    response = requests.get(
+        f"{RELEASES_URL}/{latest_version}", headers={"Authorization": authorization}
+    )
+
+    if response.status_code != 200:
+        print("Failed to get artifacts", sys.stderr)
+        print(response.status_code, sys.stderr)
+        print(response.content, sys.stderr)
+        print(response.headers, sys.stderr)
+        return
+
+    content = response.json()
+    download_url = content["artifacts"][0]["download_url"]
+    filename = content["artifacts"][0]["filename"]
+    response = requests.get(download_url, stream=True)
+
+    with open(filename, "wb") as f:
+        for chunk in response.iter_content(chunk_size=256):
+            f.write(chunk)
+
+    version_file.write_text(latest_version)
 
 
 if __name__ == "__main__":
